0c4a87692b385367fbd5b7e7506c5659f0951a59
ColinTr <56585263+ColinTr@users.noreply.github.com>
2020-06-05 10:56:12 +0200

    animation des plantes et des podoboos

--- piranhaplant.cpp
+++ piranhaplant.cpp
@@ -1,5 +1,7 @@
 #include "piranhaplant.h"
 
+#include "fireball.h"
+
 PiranhaPlant::PiranhaPlant()
 {
     textures[0] = loadTexture(":/resources/graphics/mobs/piranha/piranha-0.png").scaled(1.3*16/24*BLOCSIZE, 1.3*BLOCSIZE, Qt::KeepAspectRatio);
@@ -11,14 +13,111 @@ PiranhaPlant::PiranhaPlant()
     setHitboxEntityHeight(1.3*BLOCSIZE);
 
     setCurrentTexture(textures[0]);
+
+    animationTimer.start();
+    textureIndex = 0;
+
+    amplitude = 2*BLOCSIZE;
+    travelDuration = 2000;
+    travelDistance = amplitude / travelDuration;
+
+    collidable = true;
+    solid = false;
+
+    zValue = 1;
+
+    piranhaPlantState = PIRANHAGOINGUP;
 }
 
 void PiranhaPlant::advance()
 {
+    if(!travelTimer.isValid()){
+        travelTimer.start();
+    }
 
+    if(travelTimer.elapsed() > travelDuration){
+        switch(piranhaPlantState){
+        case PIRANHAGOINGDOWN:
+            piranhaPlantState = PIRANHAWAITINGDOWN;
+            break;
+        case PIRANHAGOINGUP:
+            piranhaPlantState = PIRANHAWAITINGUP;
+            break;
+        case PIRANHAWAITINGDOWN:
+            piranhaPlantState = PIRANHAGOINGUP;
+            break;
+        case PIRANHAWAITINGUP:
+            piranhaPlantState = PIRANHAGOINGDOWN;
+            break;
+        default:
+            break;
+        }
+
+        travelTimer.restart();
+    }
+    else{
+        switch(piranhaPlantState){
+        case PIRANHAGOINGDOWN:
+            moveTo(spawnPosition.x(), spawnPosition.y() + travelDistance * travelTimer.elapsed());
+            break;
+        case PIRANHAGOINGUP:
+            moveTo(spawnPosition.x(), spawnPosition.y() + amplitude - travelDistance * travelTimer.elapsed());
+            break;
+        case PIRANHAWAITINGDOWN:
+            break;
+        case PIRANHAWAITINGUP:
+            break;
+        default:
+            break;
+        }
+    }
 }
 
 void PiranhaPlant::animate()
 {
+    if(animationTimer.elapsed() > animationDelay){
+        animationTimer.restart();
+        if(textureIndex > 1){
+            textureIndex = 0;
+        }
 
+        setCurrentTexture(textures[textureIndex]);
+
+        textureIndex++;
+    }
+}
+
+void PiranhaPlant::collisionByDefaultHandler(ObjectModel *o)
+{
+    if(dynamic_cast<FireBall *>(o)){
+        die();
+    }
+}
+
+void PiranhaPlant::collisionOnBottomHandler(ObjectModel *o)
+{
+    if(dynamic_cast<FireBall *>(o)){
+        die();
+    }
+}
+
+void PiranhaPlant::collisionOnLeftHandler(ObjectModel *o)
+{
+    if(dynamic_cast<FireBall *>(o)){
+        die();
+    }
+}
+
+void PiranhaPlant::collisionOnRightHandler(ObjectModel *o)
+{
+    if(dynamic_cast<FireBall *>(o)){
+        die();
+    }
+}
+
+void PiranhaPlant::collisionOnTopHandler(ObjectModel *o)
+{
+    if(dynamic_cast<FireBall *>(o)){
+        die();
+    }
 }
--- podoboo.h
+++ podoboo.h
@@ -10,7 +10,7 @@ private:
 
     QPointF spawnPosition;
     float amplitude; // The number of blocks it will travel before coming back
-    int travelDuration; // (in ms) The time it will take the platform to go all the way down
+    int travelDuration; // (in ms) The time it will take to go all the way down
     QElapsedTimer travelTimer;
     float travelDistance; // The distance it must travel at each frame
 
